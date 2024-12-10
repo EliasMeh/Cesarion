ebbb58bfd8582e87a8c92760699ae2fdff68f706
Elias MEHDAOUI- -JORGE <elias.mehdaoui-jorge@supdevinci-edu.fr>
2024-12-10 12:31:57 +0100

    changement

--- src/app/api/professeurs/[profid]/route.ts
+++ src/app/api/professeurs/[profid]/route.ts
@@ -1,10 +1,9 @@
-import { NextResponse } from "next/server";
+import { NextRequest, NextResponse } from "next/server";
 import { PrismaClient } from "@prisma/client";
 
 const prisma = new PrismaClient();
 
-export async function GET(request: Request, context: { params: { profid: string } }) {
-    const { params } = context;
+export async function GET(request: NextRequest, { params }: { params: { profid: string } }) {
     const profid = parseInt(params.profid, 10);
 
     const data = await prisma.professeur.findFirst({
