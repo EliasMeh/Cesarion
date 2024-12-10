163c9fda76a5585fc7af5ceabe6105b2320446f0
Elias MEHDAOUI- -JORGE <elias.mehdaoui-jorge@supdevinci-edu.fr>
2024-12-10 12:36:37 +0100

    changement

--- src/app/api/eleves/[eleveid]/route.ts
+++ src/app/api/eleves/[eleveid]/route.ts
@@ -1,9 +1,9 @@
-import { NextResponse } from "next/server";
+import { NextRequest, NextResponse } from "next/server";
 import { PrismaClient } from "@prisma/client";
 
 const prisma = new PrismaClient();
 
-export async function GET(request: Request, { params }: { params: { eleveid: string } }) {
+export async function GET(request: NextRequest, { params }: { params: { eleveid: string } }) {
     const eleveid = parseInt(params.eleveid, 10);
 
     const data = await prisma.eleve.findFirst({
